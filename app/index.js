1add244e2bf8984e5e00c5145e3f66899d15b4ea
David <david.recuencogadea@gmail.com>
2016-05-07 12:21:55 +0200

    UPDATE files, prompts, gitignores

--- app/index.js
+++ app/index.js
@@ -1,8 +1,20 @@
+const _ = require('lodash');
 const generators = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
 
-const files = [];
+const files = [
+  {
+    src: 'eslintrc',
+    dst: '.eslintrc'
+  }, {
+    src: 'gitignore',
+    dst: '.gitignore'
+  },
+  'package.json',
+  'README.md'
+];
+
 const options = {};
 
 module.exports = generators.Base.extend({
@@ -14,7 +26,52 @@ module.exports = generators.Base.extend({
       `Welcome to the good ol\' ${chalk.green('build-tool')} generator!`
     ));
   },
+  prompting: function() {
+    const questions = [
+      {
+        type: 'input',
+        name: 'projectName',
+        message: 'What\'s the name of the project?'
+      }, {
+        type: 'list',
+        name: 'builder',
+        message: 'Choose a builder!',
+        choices: [
+          'grunt',
+          'gulp'
+        ],
+        default: 'grunt'
+      }, {
+        type: 'list',
+        name: 'styles',
+        message: 'Do you wish to use a CSS preprocessor?',
+        choices: [
+          'css',
+          'less',
+          'sass',
+          'stylus'
+        ],
+        default: 'sass'
+      }
+    ];
+
+    return this.prompt(questions).then((answers) => {
+      options.projectName = answers.projectName;
+      options.builder = answers.builder;
+      options.styles = anwsers.styles;
+    });
+  },
   writing: function() {
+    files.push(`${_.capitalize(options.builder)}file.js`);
+
+    files.push({
+      src: `${options.builder}/tasks`,
+      dst: 'build/tasks'
+    }, {
+      src: `${options.builder}/custom/${options.styles}.js`,
+      dst: 'build/tasks/${options.styles}.js'
+    });
+
     files.forEach((file) => {
       let src, dst;
 
@@ -25,13 +82,13 @@ module.exports = generators.Base.extend({
         src = file.src;
         dst = file.dst;
       }
-    });
 
-    this.fs.copyTpl(
-      this.templatePath(src),
-      this.destinationPath(dst),
-      options
-    );
+      this.fs.copyTpl(
+        this.templatePath(src),
+        this.destinationPath(dst),
+        options
+      );
+    });
   },
   end: function() {
     this.log('All done, time to build!');
